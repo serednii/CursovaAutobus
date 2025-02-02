d267f962f599d53de340592a771ef96a3a4cb2ee
Serednii Mykola <seredniimykola@gmail.com>
2025-02-02 21:58:06 +0100

    delete folder passenger and driver

--- app/driver/myroutes/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function MyRoutes(){
-    return (<div>My Routes</div>)
-}
--- app/passenger/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Passenger(){
-    return(
-        <div className="">
-            main passenger
-        </div>
-    )
-}
