c10aa1069b36e1708ae3d7aeb2e756127af88fd7
Serednii Mykola <seredniimykola@gmail.com>
2025-02-16 22:42:05 +0100

    change style CheckboxOptionsMain

--- components/shared/form/CheckboxOptionsMain.tsx
+++ components/shared/form/CheckboxOptionsMain.tsx
@@ -16,86 +16,51 @@ interface Props {
 
 export default function CheckboxOptionsMain({ register, watch, reset }: Props) {
   return (
-    <div className="flex justify-between flex-wrap">
-      {/* <Typography variant="h6" gutterBottom>
-        <FormControlLabel
-          control={
-            <Checkbox
-              {...register("isOption")}
-              checked={watch("isOption") || false} // Переконайтеся, що значення не `undefined`
-              onChange={(e) => {
-                const checked = e.target.checked;
-                reset({
-                  ...watch(),
-                  isOption: checked,
-                  wifi: checked,
-                  coffee: checked,
-                  power: checked,
-                  restRoom: checked,
-                });
-              }}
-            />
-          }
-          label={<div style={{ display: "flex", alignItems: "center" }}> Additional options:</div>}
-        />
-      </Typography> */}
-      <div>
-        <FormGroup>
-          <FormControlLabel
-            control={<Checkbox {...register("wifi")} />}
-            // {...register("wifi")}
-            checked={watch("wifi") || false}
-            disabled={!watch("isOption")}
-            label={
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <IoIosWifi style={{ marginRight: "8px", fontSize: "24px" }} />
-                Wi-Fi
-              </div>
-            }
-          />
-          <FormControlLabel
-            control={<Checkbox {...register("coffee")} />}
-            // {...register("coffee")}
-            disabled={!watch("isOption")}
-            checked={watch("coffee") || false}
-            label={
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <CgCoffee style={{ marginRight: "8px", fontSize: "24px" }} />
-                Coffee/Tea
-              </div>
-            }
-          />
-        </FormGroup>
-      </div>
-      <div>
-        <Typography variant="h6" gutterBottom></Typography>
-        <FormGroup>
-          <FormControlLabel
-            control={<Checkbox {...register("power")} />}
-            // {...register("power")}
-            disabled={!watch("isOption")}
-            checked={watch("power") || false}
-            label={
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <MdOutlinePower style={{ marginRight: "8px", fontSize: "24px" }} />
-                Power Outlets
-              </div>
-            }
-          />
-          <FormControlLabel
-            control={<Checkbox {...register("restRoom")} />}
-            // {...register("restRoom")}
-            disabled={!watch("isOption")}
-            checked={watch("restRoom") || false}
-            label={
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <FaRestroom style={{ marginRight: "8px", fontSize: "24px" }} />
-                RestRoom
-              </div>
-            }
-          />
-        </FormGroup>
-      </div>
+    <div className="grid grid-cols-1 justify-self-center min-[900px]:justify-self-start min-[500px]:grid-cols-2 min-[900px]:grid-cols-4 gap-4">
+      <FormControlLabel
+        control={<Checkbox {...register("wifi")} />}
+        checked={watch("wifi") || false}
+        disabled={!watch("isOption")}
+        label={
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <IoIosWifi style={{ marginRight: "8px", fontSize: "24px" }} />
+            Wi-Fi
+          </div>
+        }
+      />
+      <FormControlLabel
+        control={<Checkbox {...register("coffee")} />}
+        disabled={!watch("isOption")}
+        checked={watch("coffee") || false}
+        label={
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <CgCoffee style={{ marginRight: "8px", fontSize: "24px" }} />
+            Coffee/Tea
+          </div>
+        }
+      />
+      <FormControlLabel
+        control={<Checkbox {...register("power")} />}
+        disabled={!watch("isOption")}
+        checked={watch("power") || false}
+        label={
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <MdOutlinePower style={{ marginRight: "8px", fontSize: "24px" }} />
+            Power Outlets
+          </div>
+        }
+      />
+      <FormControlLabel
+        control={<Checkbox {...register("restRoom")} />}
+        disabled={!watch("isOption")}
+        checked={watch("restRoom") || false}
+        label={
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <FaRestroom style={{ marginRight: "8px", fontSize: "24px" }} />
+            RestRoom
+          </div>
+        }
+      />
     </div>
   );
 }
