07ab50337c77550b3dad767a43d9e47520ea8658
Serednii Mykola <seredniimykola@gmail.com>
2025-02-19 21:39:54 +0100

    fix TableSearchRoutes

---
+++ components/shared/passenger/TableSearchRoutes.tsx
@@ -0,0 +1,93 @@
+"use client";
+import * as React from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+
+import { useRouter } from "next/navigation";
+import { TypeBaseRoute } from "@/types/route-passenger.types";
+
+// const paginationModel = { page: 0, pageSize: 5 };
+
+interface Props {
+  routes: TypeBaseRoute[];
+  status: "authenticated" | "loading" | "unauthenticated";
+}
+
+export default function TableSearchRoutes({ routes, status }: Props) {
+  const router = useRouter();
+
+  const handleViewRoute = (route: TypeBaseRoute) => {
+    if (status !== "authenticated") {
+      sessionStorage.setItem("idRoute", String(route.id));
+      sessionStorage.setItem("transition", "seatselection");
+    }
+    router.push(`/seatselection/${route.id}`);
+  };
+
+  // Основні колонки без колонки againRouter
+  const columns: GridColDef[] = [
+    {
+      field: "departureDate",
+      headerName: "Departure Date",
+      minWidth: 200,
+      flex: 1,
+    },
+    {
+      field: "arrivalDate",
+      headerName: "Arrival Date",
+      minWidth: 200,
+      flex: 1,
+    },
+    {
+      field: "departureFrom",
+      headerName: "From",
+      minWidth: 130,
+      flex: 1,
+    },
+    {
+      field: "arrivalTo",
+      headerName: "To",
+      minWidth: 130,
+      flex: 1,
+    },
+    {
+      field: "availableSeats",
+      headerName: "Available Seats",
+      minWidth: 100,
+      flex: 1,
+    },
+    {
+      field: "routePrice",
+      headerName: "Price",
+      minWidth: 50,
+      flex: 1,
+    },
+    {
+      field: "viewRouter",
+      headerName: "View Route",
+      minWidth: 130,
+      flex: 1,
+      sortable: false,
+      renderCell: (params) => (
+        <Button variant="contained" color="primary" size="small" onClick={() => handleViewRoute(params.row)}>
+          Book Now
+        </Button>
+      ),
+    },
+  ];
+
+  // Додаємо колонку againRouter, якщо isRouteAgain === true
+
+  return (
+    <Paper sx={{ width: "100%" }}>
+      <DataGrid
+        rows={routes}
+        columns={columns}
+        // initialState={{ pagination: { paginationModel } }}
+        // pageSizeOptions={[5, 10]}
+        sx={{ border: 0 }}
+      />
+    </Paper>
+  );
+}
