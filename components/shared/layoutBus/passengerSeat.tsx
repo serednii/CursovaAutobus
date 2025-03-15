b4aac6d2c4f36bfa42be7e4d5b2c1bc9bf9c13d6
Serednii Mykola <seredniimykola@gmail.com>
2025-03-15 08:32:11 +0100

    create components SeatButton, SubPassengerFields

--- components/shared/layoutBus/passengerSeat.tsx
+++ components/shared/layoutBus/passengerSeat.tsx
@@ -3,7 +3,8 @@ import { RoleEnum, SeatStatusEnum } from "@/enum/shared.enums";
 import { cn } from "@/lib/utils";
 import { ILayoutData, BusSeatInfo } from "@/types/layoutbus.types";
 import { UserSession } from "@/types/next-auth";
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
+import SeatButton from "./SeatButton";
 // import SeatButton from "./SeatButton";
 
 interface Props {
@@ -17,14 +18,15 @@ interface Props {
   driverId: number | null | undefined;
 }
 
-export default function PassengerSeat(props: Props) {
+export default memo(function PassengerSeat(props: Props) {
+  console.log("PassengerSeat RENDER");
   const { className, params, user, dataLayoutBus, setDataLayoutBus, sessionUser, action, driverId } = props;
   const { number } = params;
   const [changeStatus, setChangeStatus] = useState<BusSeatInfo>(() => params);
   const keys = Object.keys(params) as (keyof typeof params)[];
   const styles: React.CSSProperties = {};
   const sessionUserId = Number(sessionUser?.id) as number;
-  console.log("sessionUserId === driverId", sessionUserId, driverId);
+  // console.log("sessionUserId === driverId", sessionUserId, driverId);
   //add styles top, bottom, left, right
   keys.forEach((key) => {
     if (params[key] && key !== "number" && key !== "busSeatStatus" && key !== "passenger") {
@@ -72,26 +74,27 @@ export default function PassengerSeat(props: Props) {
   };
 
   return (
-    <button
-      disabled={changeStatus.busSeatStatus === SeatStatusEnum.RESERVED || (action === RoleEnum.PASSENGER && sessionUserId === driverId)}
-      onClick={handleClick}
-      style={styles}
-      className={cn("absolute disabled:cursor-not-allowed", className)}
-    >
-      <div className={cn("relative w-[60px] h-[40px] rounded-t-lg rounded-b-md flex justify-center items-center", statusColor)}>
-        <p className="text-white text-[1.5rem] translate-x-[-5px]">{number}</p>
-        <div className="absolute right-[2px] top-[0px] rounded-t-md rounded-b-xl w-[15px] h-[40px] bg-[#5a8950]"></div>
-      </div>
-    </button>
-    // <SeatButton
-    //   number={number}
-    //   changeStatus={changeStatus}
-    //   action={action}
-    //   driverId={driverId}
-    //   sessionUserId={sessionUserId}
-    //   handleClick={handleClick}
-    //   className={className}
-    //   statusColor={statusColor}
-    // />
+    // <button
+    //   disabled={changeStatus.busSeatStatus === SeatStatusEnum.RESERVED || (action === RoleEnum.PASSENGER && sessionUserId === driverId)}
+    //   onClick={handleClick}
+    //   style={styles}
+    //   className={cn("absolute disabled:cursor-not-allowed", className)}
+    // >
+    //   <div className={cn("relative w-[60px] h-[40px] rounded-t-lg rounded-b-md flex justify-center items-center", statusColor)}>
+    //     <p className="text-white text-[1.5rem] translate-x-[-5px]">{number}</p>
+    //     <div className="absolute right-[2px] top-[0px] rounded-t-md rounded-b-xl w-[15px] h-[40px] bg-[#5a8950]"></div>
+    //   </div>
+    // </button>
+    <SeatButton
+      number={number}
+      changeStatus={changeStatus}
+      action={action}
+      sessionUserId={sessionUserId}
+      driverId={driverId}
+      statusColor={statusColor}
+      styles={styles}
+      className={className}
+      handleClick={handleClick}
+    />
   );
-}
+});
