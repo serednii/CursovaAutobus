795d981776e0336d3b71dc92ad45c34e1db16def
Serednii Mykola <seredniimykola@gmail.com>
2025-03-25 00:30:00 +0100

    add useGetRoute inseatselection

--- app/(passenger)/seatselection/[id]/page.tsx
+++ app/(passenger)/seatselection/[id]/page.tsx
@@ -11,19 +11,18 @@ import busStore from "@/mobx/busStore";
 import { observer } from "mobx-react-lite";
 import useMixedLayoutsSeatsData from "@/components/shared/passenger/useBusLayoutData";
 import { useGetSessionParams } from "@/hooks/useGetSessionParams";
+import useGetRoute from "./usegetRoute";
+import useChangeBusSeatStatus from "./changeBusSeatStatus";
 
 function SeatSelection() {
   const { sessionUser, userSessionId, session } = useGetSessionParams();
 
   const params = useParams();
   const id = params.id ? Number(params.id) : 0;
-  const [route, setRoute] = useState<IGetRouteSeatSelection | null>(null);
-  const [loading, setLoading] = useState(true);
 
   console.log("id in SeatSelection ", id);
   console.log("userSessionId in SeatSelection ", userSessionId);
   console.log("session in SeatSelection ", session);
-  console.log("route in SeatSelection ", route);
 
   useEffect(() => {
     busStore.setDataLayoutBus(null, RoleEnum.PASSENGER);
@@ -34,42 +33,15 @@ function SeatSelection() {
       busStore.setUserIdSession(userSessionId);
     }
   }, [userSessionId, busStore.setUserIdSession]);
-
-  useEffect(() => {
-    if (!id || !userSessionId) return;
-    const getRouteById = async () => {
-      try {
-        const routeArray = await fetchGetRoutesById.searchRoute([id], selectSeatSelection, "seatSelection");
-        const routes = routeArray as IGetRouteSeatSelection[] | null;
-        const fetchedRoute = routes?.[0] || null;
-        console.log("fetchedRoute in SeatSelection ", fetchedRoute);
-
-        if (fetchedRoute && fetchedRoute.busSeats) {
-          const updatedPassengers = fetchedRoute.busSeats.map((seat) =>
-            seat.passenger === userSessionId && seat.busSeatStatus === SeatStatusEnum.RESERVED
-              ? { ...seat, busSeatStatus: SeatStatusEnum.SELECTED }
-              : seat
-          );
-          fetchedRoute.busSeats = updatedPassengers;
-        }
-
-        console.log("fetchedRoute in SeatSelection ", fetchedRoute);
-        setRoute(fetchedRoute);
-      } catch (error) {
-        console.error("Error fetching route:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getRouteById();
-  }, [id, userSessionId]);
+  const { route, loading } = useGetRoute({ userSessionId, id });
+  useChangeBusSeatStatus({ route, userSessionId });
 
   useMixedLayoutsSeatsData(route);
 
   if (loading) return <div>Loading...</div>;
   if (!route || !session || !params) return <div>No route found</div>;
   console.log("route in SeatSelection ", route);
+
   return (
     <Container className="pt-4">
       <SelectedBusInfo route={route} />
