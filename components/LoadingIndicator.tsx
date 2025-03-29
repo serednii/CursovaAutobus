e3e8b4bf8e5c849f6cebf4a8aa2d82884eb8834e
Serednii Mykola <seredniimykola@gmail.com>
2025-03-29 12:43:33 +0100

    add LoadingIndicator

---
+++ components/LoadingIndicator.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { CircularProgress } from "@mui/material";
+
+export default function LoadingIndicator() {
+  return <CircularProgress className="absolute top-2 left-1/2 color-[#94f07c] z-10" size={30} />;
+}
