49e7fc5fb7b64cf30fc418e04b4823a94e1c3a39
Serednii Mykola <seredniimykola@gmail.com>
2025-04-05 11:53:40 +0200

    change styles container

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -6,6 +6,8 @@ interface Props {
 }
 
 export const Container: React.FC<React.PropsWithChildren<Props>> = ({ className, children }) => {
-  return <div className={cn("mx-auto max-w-[1920px] px-4", className)}>{children}</div>;
+  return (
+    <div className={cn("mx-auto max-w-[1920px] px-1 sm:px-2 md:px-4", className)}>{children}</div>
+  );
   // return <div className={cn('mx-auto max-w-[1280px] px-4', className)}>{children}</div>;
 };
